ab97f4d05a7f41a390cfba5f287e2e7b86547ef9
Pratham <pratham.webdev@gmail.com>
2021-08-26 20:12:10 +0530

    added delete functionality

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,18 +6,26 @@ import Footer from "./footer";
 import notes from "../notes";
 
 function App() {
-  
+
   const [notesArray, addNotes] = useState(notes);
 
   function updateNotes(inputItem) {
     addNotes([...notesArray, inputItem]);
   }
 
+  function deleteNotes(id){
+    addNotes(prevalue =>{
+      return prevalue.filter((el,index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
       <Input updateNotes={updateNotes} />
-      <NotesCollection notesArray={notesArray} />
+      <NotesCollection notesArray={notesArray} deleteNotes={deleteNotes} />
       <Footer />
     </div>
   );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function Note(props) {
   return (
     <div className="col">
-    <div className="card text-white bg-dark border-lt">
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">
-          {props.content}
-        </p>
+      <div className="card text-white bg-dark border-lt">
+        <div className="card-body">
+         <div className="d-flex justify-content-between align-items-baseline pb-2">
+         <h5 className="card-title m-0 ">{props.title}</h5>
+          <button
+            onClick={() => {
+              props.deleteNotes(props.id);
+            }}
+            className="btn btn-sm btn-main"
+          >
+           ✖
+          </button>
+         </div>
+          <p className="card-text">{props.content}</p>
+          
+        </div>
       </div>
-    </div>
     </div>
   );
 }
