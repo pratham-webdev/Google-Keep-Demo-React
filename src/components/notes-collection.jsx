aed9afa51dda7f4b151aa48736f75ca9c82f98a0
Pratham <pratham.webdev@gmail.com>
2021-08-26 20:00:30 +0530

    fixes

--- src/components/notes-collection.jsx
+++ src/components/notes-collection.jsx
@@ -5,8 +5,8 @@ function NotesCollection(props) {
   return (
     <div className="py-5 container">
       <div className="row row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-2 g-lg-3">
-        {props.notesArray.map((item) => (
-          <Note key={item.key} title={item.title} content={item.content} />
+        {props.notesArray.map((item, index) => (
+          <Note key={index} title={item.title} content={item.content} />
         ))}
       </div>
     </div>
